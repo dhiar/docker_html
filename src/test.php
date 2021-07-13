6c191dcdddc37426efe0669892c825b5314107a8
dhiar praditya <darrenzie@gmail.com>
2021-07-13 16:43:35 +0700

    change port nginx to 8085

--- src/test.php
+++ src/test.php
@@ -1,5 +1,5 @@
 <?php
   echo "<h1>It Works</h1><br>";
   echo "<h3>~/Documents/Project/docker_project_web. Branch:docker_html</h3><br>";
-  echo "<h3>http://localhost:8080/test.php</h3><br>";
+  echo "<h3>http://localhost:8085/test.php</h3><br>";
 ?>
