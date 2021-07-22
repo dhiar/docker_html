0fccd8854e17db8305488e3cefc11b13cc2c9654
dhiar praditya <darrenzie@gmail.com>
2021-07-22 16:03:31 +0700

    swagger UI

--- README.md
+++ README.md
@@ -24,3 +24,13 @@ Remove volume yang tidak digunakan
 ```
 docker volume prune
 ```
+
+Example POST /prediction data
+```
+[
+    {
+        "category": "talk.politics.misc",
+        "text": "Angela Merkel just walked into her fourth term as chancellor of Germany.Her party, the Christian Democrats (CDU), picked up 32.5 percent of the votes in Sunday's election, according to the first exit polls issued at 6 pm German local time."
+    }
+]
+```
--- app.py
+++ app.py
@@ -1,91 +1,15 @@
-import mysql.connector
-import json
 from flask import Flask
 import connexion
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#   return 'Hello, Docker Update Dhiar sss!'
-
-def post_predictions(query):
-  predictions = []
+def post_users(query):
+  users = []
   return query
 
-# @app.route('/widgets')
-# def get_widgets() :
-#   mydb = mysql.connector.connect(
-#     host="mysqldb",
-#     user="root",
-#     password="manager247",
-#     database="inventory"
-#   )
-#   cursor = mydb.cursor()
+options = {"swagger_ui": True}
+app = connexion.FlaskApp(__name__, specification_dir='openapi/',
+  options=options)
 
-
-#   cursor.execute("SELECT * FROM widgets")
-
-#   row_headers=[x[0] for x in cursor.description] #this will extract row headers
-
-#   results = cursor.fetchall()
-#   json_data=[]
-#   for result in results:
-#     json_data.append(dict(zip(row_headers,result)))
-
-#   cursor.close()
-
-#   return json.dumps(json_data)
-
-# @app.route('/initdb')
-# def db_init():
-#   mydb = mysql.connector.connect(
-#     host="mysqldb",
-#     user="root",
-#     password="manager247"
-#   )
-#   mycursor = mydb.cursor()
-#   mycursor.execute("DROP DATABASE IF EXISTS inventory")
-#   mycursor.execute("CREATE DATABASE inventory")
-#   mydb.commit()
-
-#   mydb = mysql.connector.connect(
-#     host="mysqldb",
-#     user="root",
-#     password="manager247",
-#     database="inventory"
-#   )
-#   mycursor = mydb.cursor()
-#   mycursor.execute("CREATE TABLE widgets (id int NOT NULL AUTO_INCREMENT, name VARCHAR(255), description VARCHAR(255), PRIMARY KEY (id))")
-#   mydb.commit()
-
-#   mydb = mysql.connector.connect(
-#     host="mysqldb",
-#     user="root",
-#     password="manager247",
-#     database="inventory"
-#   )
-#   mycursor = mydb.cursor()
-  
-#   sql = "INSERT INTO widgets (name, description) VALUES (%s, %s)"
-#   val = [
-#     ("widgets_1", "description_1"),
-#     ("widgets_2", "description_2")
-#   ]
-#   mycursor.executemany(sql, val)
-
-#   mydb.commit()
-#   return 'init database'
-
-# if __name__ == "__main__":
-#   app.run(host ='0.0.0.0')
-
-
-
-# ----------------
-
-app = connexion.App(__name__)
-app.add_api('swagger.yaml')
+app.add_api('swagger.yaml', arguments={'api_local': 'local_value'})
 
 if __name__ == '__main__':
   app.run(port=5000)
